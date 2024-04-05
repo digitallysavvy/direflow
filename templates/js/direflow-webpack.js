e3e8d738d231a02d5f66df31a55d6581406aaaab
digitallysavvy <hermes.frangoudis@gmail.com>
2024-04-05 18:02:27 -0400

    updated build for testing

--- templates/js/direflow-webpack.js
+++ templates/js/direflow-webpack.js
@@ -1,10 +1,18 @@
 const { webpackConfig } = require('direflow-scripts');
 
-/**
- * Webpack configuration for Direflow Component
- * Additional webpack plugins / overrides can be provided here
- */
-module.exports = (config, env) => ({
-  ...webpackConfig(config, env),
-  // Add your own webpack config here (optional)
-});
+module.exports = (config, env) => {
+  const direflowWebpackConfig = webpackConfig(config, env);
+
+  return {
+    ...direflowWebpackConfig,
+    output: {
+      ...direflowWebpackConfig.output,
+      // Ensure the module format is set to CommonJS for Node.js compatibility
+      libraryTarget: 'commonjs2',
+    },
+    // Targeting Node.js, we don'o't want Webpack to polyfill or mock certain Node.js globals and built-in modules
+    target: 'node',
+    // Exclude 'node_modules' from the output bundle
+    externals: [...(direflowWebpackConfig.externals || []), /node_modules/],
+  };
+};
